a19d26e2e6b5fd7de4fc0911e5e810c40232cb86
gnsqud24@naver.com <gnsqud24@naver.com>
2022-10-05 23:59:59 +0900

    스플래시 화면 구현

---
+++ app/src/main/java/org/techtown/mysololife/SplashActivity.kt
@@ -0,0 +1,22 @@
+package org.techtown.mysololife
+
+/*스플래쉬 화면입니다.*/
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import org.techtown.mysololife.auth.IntroActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //스플래쉬 끝나고 이동하는 화면
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))//IntroActivity로 이동
+            finish()//스플래시 화면 종료
+        },3000)//3초 뒤에 이동
+    }
+}
---
+++ app/src/main/java/org/techtown/mysololife/auth/IntroActivity.kt
@@ -0,0 +1,16 @@
+package org.techtown.mysololife.auth
+/*스플래쉬 화면 다음에 뜨는 인트로 화면입니다.*/
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.techtown.mysololife.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+
+
+    }
+}
